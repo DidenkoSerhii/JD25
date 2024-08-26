eb3d16f3b4face7b142ccb7c35e22ec7fd98b0d7
DidenkoSerhii <didenkosergey1005@gmail.com>
2024-08-26 17:03:40 +0300

    Initial commit

---
+++ src/main/java/org/example/FibonacciDynamicProgramming.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class FibonacciDynamicProgramming {
+    public static int fibonacci(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
